1d04cb9aa68f279417e05ab02827b4dfb524c168
John Howie <jkhowie63@gmail.com>
2016-09-26 16:50:13 -0400

    made team reference on user optional

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :business_processes
-  belongs_to :team
+  belongs_to :team, optional: true 
   def self.from_omniauth(auth)
    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
      user.provider = auth.provider
