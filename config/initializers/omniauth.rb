934c5729ae1e1ac8f1e808872a38cea659fbda41
John Howie <jkhowie63@gmail.com>
2016-09-26 16:17:04 -0400

    added google login

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,10 @@
+OmniAuth.config.logger = Rails.logger
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, Figaro.env.google_client_id, Figaro.env.google_client_secret, {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+#  provider :google_oauth2, '625104604997-jot88nivuidg6ntmf3hnlrfdeg7cvdcd.apps.googleusercontent.com', 'ToojjBOLb9vUwTFrdZ6HC_g0', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+
+end
+
+
+#'570001027303-n8qq540ubuo66hddec2jlkkstmd07rd5.apps.googleusercontent.com'
+#google_client_secret: '5OJdbwFoWBsbnGBMJbbDwAmB'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
+  root 'work_items#index'
   resources :work_items
   resources :process_flows
   resources :teams
   resources :customers
   resources :users
+
+
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+
+  resources :sessions, only: [:create, :destroy]
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
