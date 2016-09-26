b9d2ea6daf8bab02cb6d89a14757deeb02914d00
Mujib Kunju <mujibmk@gmail.com>
2016-09-26 15:14:06 -0400

    Created BusinessProcess model.

---
+++ app/models/business_process.rb
@@ -0,0 +1,3 @@
+class BusinessProcess < ApplicationRecord
+  belongs_to :user
+end
---
+++ db/migrate/20160926191341_create_business_processes.rb
@@ -0,0 +1,10 @@
+class CreateBusinessProcesses < ActiveRecord::Migration[5.0]
+  def change
+    create_table :business_processes do |t|
+      t.string :title
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
