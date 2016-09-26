b8f6afd8c5e0787ad70c9ffc3a24ec0e7b79422a
John Howie <jkhowie63@gmail.com>
2016-09-26 17:10:20 -0400

    added param to google login to work around their bug

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, Figaro.env.google_client_id, Figaro.env.google_client_secret, {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, Figaro.env.google_client_id, Figaro.env.google_client_secret, {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}, skip_jwt: true }}
 #  provider :google_oauth2, '625104604997-jot88nivuidg6ntmf3hnlrfdeg7cvdcd.apps.googleusercontent.com', 'ToojjBOLb9vUwTFrdZ6HC_g0', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 
 end
