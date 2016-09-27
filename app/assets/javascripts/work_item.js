c8611eb66d4fc9ff7be26cf2b1e2d26d431f07b5
John Howie <jkhowie63@gmail.com>
2016-09-27 09:53:16 -0400

    added js for button on work item index and removed work_item.coffee

---
+++ app/assets/javascripts/work_item.js
@@ -0,0 +1,18 @@
+$(function(){
+  $('#queue-back').on('click', function(){
+    alert('back button pressed')
+  });
+
+  $('#queue-escolate').on('click', function(){
+    alert('Escolate button pressed')
+  });
+
+  $('#queue-forward').on('click', function(){
+    alert('forward button pressed')
+  });
+
+  $('#queue-save').on('click', function(){
+    alert('save button pressed')
+  });
+
+});
