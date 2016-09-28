3249bdb6409e72286b394a90cd4cc6b77794ee9e
viktor <v.xhelilaj@gmail.com>
2016-09-28 13:50:39 -0400

    changes to team edit, button and css effects

---
+++ app/assets/javascripts/teams.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+  $('#addToTeam').on('click', function(e){
+
+    e.preventDefault();
+
+    $('.user_specs').append('<div class="sub"><input> </input></div>');
+
+
+
+  });
+
+});
