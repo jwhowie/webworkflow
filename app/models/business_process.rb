003630d790be08e3c3b70f196514391463ab9345
Mujib Kunju <mujibmk@gmail.com>
2016-09-28 09:20:57 -0400

    Added associations to the business_process model and process_flow model.

--- app/models/business_process.rb
+++ app/models/business_process.rb
@@ -1,3 +1,4 @@
 class BusinessProcess < ApplicationRecord
   belongs_to :user
+  has_many :process_flows
 end
--- app/models/process_flow.rb
+++ app/models/process_flow.rb
@@ -1,5 +1,6 @@
 class ProcessFlow < ApplicationRecord
     belongs_to :team
-    has_many :work_items
+    belongs_to :business_process
+    has_many :work_items    
 
 end
