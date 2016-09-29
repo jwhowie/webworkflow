64fbc141cf83814fe44dba5e31b465a62061b4d1
viktor <v.xhelilaj@gmail.com>
2016-09-29 17:22:12 -0400

    sharing for pair-programming on teams process

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -9,13 +9,15 @@ $(document).ready(function(){
     $('.dynamic').data('latest-user-id', nextUserId);
 
 
-    $('.dynamic').append("<div style='clear: both'><div class='user_specs'><input type='text' name='peers["+nextUserId+"][name]' id='team_users_" + nextUserId + "_name'><input type='text' name='peers[" + nextUserId + "][email]' id='team_users_" + nextUserId + "_email'></div></div>");
+    $('.dynamic').append("<div style='clear: both'><div class='user_specs'><input type='text' name='peers["+nextUserId+"][name]' id='team_users_" + nextUserId + "_name'><input type='text' name='peers[" + nextUserId + "][email]' id='team_users_" + nextUserId + "_email'><button id='removeFromTeam'>-</button></div></div>");
   });
 
-});
+  $('#removeFromTeam').on('click', function(e2){
 
-// <div class="user_specs" data-user-id=0>
-//   <%= uf.label :name %>
-//   <%= uf.text_field :name, {name: "users[0][name]"} %>
-//
-// </div>
+    e2.preventDefault();
+
+    var lastUserId = $('.user_specs');
+
+    $('.dynamic').remove("<div style='clear: both'><div class='user_specs'><input type='text' name='peers["+nextUserId+"][name]' id='team_users_" + nextUserId + "_name'><input type='text' name='peers[" + nextUserId + "][email]' id='team_users_" + nextUserId + "_email'></div></div>");
+  });
+});
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -25,7 +25,8 @@ class TeamsController < ApplicationController
   # POST /teams.json
   def create
     @team = Team.new(team_params)
-
+    @team.user = current_user
+# raise params.inspect
     respond_to do |format|
       if @team.save
         format.html { redirect_to @team, notice: 'Team was successfully created.' }
