0e2b6a78c597777c8a48186e2fbaf0ed3f3cd389
John Howie <jkhowie63@gmail.com>
2016-10-03 09:33:41 -0400

    added team to menu if current user is a team leader and scroll on queue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_customer_item
   helper_method :current_user
+  helper_method :my_team
+
+  def my_team
+    current_user.team
+  end
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
