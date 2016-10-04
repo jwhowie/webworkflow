f499283c1faf7416e99fbdbd7f1594e5dd841731
Mujib Kunju <mujibmk@gmail.com>
2016-10-04 15:06:54 -0400

    Changes to Teams, Process Flows.

--- app/models/process_flow.rb
+++ app/models/process_flow.rb
@@ -1,6 +1,14 @@
 class ProcessFlow < ApplicationRecord
     belongs_to :team
     belongs_to :business_process
-    has_many :work_items    
+    has_many :work_items
+
+
+    def self.my_process_flows(current_user)
+      query = "select process_flows.id, step_number, step_name, team_id from process_flows, teams where team_id = teams.id"
+
+      return result = ActiveRecord::Base.connection.execute(query).to_json
+
+    end
 
 end
